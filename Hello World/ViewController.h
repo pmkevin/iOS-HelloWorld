87f17e56cfaae68dbf173226210dbe328d43cc89
pmkevin <yangkevin@outlook.com>
2015-02-23 22:41:16 +0800

    Initial Commit

---
+++ Hello World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Kevin Yang on 2/23/15.
+//  Copyright (c) 2015 Kevin Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hello World/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Kevin Yang on 2/23/15.
+//  Copyright (c) 2015 Kevin Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
